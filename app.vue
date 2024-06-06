730fce272ec709392c14d802bfe1e5093357db44
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-06 15:06:38 +0200

    Form, add and remove todo from list

--- app.vue
+++ app.vue
@@ -1,11 +1,27 @@
 <script setup>
 import { ref } from 'vue'
 
-const inputText = ref('')
 const count = ref(0)
 
 function increment() {
   count.value++
+}
+
+let id = 0
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+const newTodo = ref('')
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
 }
 </script>
 
@@ -14,7 +30,17 @@ function increment() {
     Hello World no {{ count }}.
   </div>
   
-  <input v-model="inputText" />
-  <p v-if="inputText">Text is: {{ inputText }}</p>
-  <p v-else>Text is empty</p>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <p v-if="newTodo">New todo is: {{ newTodo }}</p>
+  <p v-else>New todo is empty</p>
+
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
